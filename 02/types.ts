53805c80dbed3c2c9b5adef8cd377d82a3b6ea39
Michal Radomski <michalra@op.pl>
2022-10-08 21:11:06 +0200

    Exercise 02 done

---
+++ 02/types.ts
@@ -0,0 +1,16 @@
+const today = new Date();
+today.getMonth();
+today.getTime();
+
+console.log({ today });
+console.log(today.getMonth(), today.getTime());
+
+const person = {
+  age: 20,
+};
+console.log({ person });
+
+class Color {}
+const red = new Color();
+
+console.log({ red });
