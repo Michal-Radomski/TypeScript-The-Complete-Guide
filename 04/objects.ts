0928a4067daf59182634e79c4d3fbe0d52d7f66f
Michal Radomski <michalra@op.pl>
2022-10-09 01:19:00 +0200

    Exercise 04 done

---
+++ 04/objects.ts
@@ -0,0 +1,19 @@
+const profile = {
+  name1: "alex",
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+
+const { age, name1 }: { age: number; name1: string } = profile;
+console.log({ age, name1 });
+
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
+console.log({ lat, lng });
