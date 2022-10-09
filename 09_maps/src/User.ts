5467bd9f7a391df187efbf0d199967889786ac66
Michal Radomski <michalra@op.pl>
2022-10-10 00:59:29 +0200

    Exercise 09 - map - commit 1

---
+++ 09_maps/src/User.ts
@@ -0,0 +1,18 @@
+// import { faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker/locale/pl";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
