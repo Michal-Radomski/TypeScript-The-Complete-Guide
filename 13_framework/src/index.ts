75d6c78c45c6eb2795676f6c64d770c1528dbaa9
Michal Radomski <michalra@op.pl>
2022-10-13 00:40:08 +0200

    Exercise 13 - commit2

--- 13_framework/src/index.ts
+++ 13_framework/src/index.ts
@@ -1,1 +1,14 @@
-console.log("test");
+// console.log("test");
+
+import { User } from "./model/User";
+
+const user = new User({ name: "Michal", age: 20 });
+
+// user.set({ name: "newName", age: 99 });
+
+// console.log("user:", user);
+
+user.on("change", () => {});
+user.on("hover", () => {});
+
+console.log("user:", user);
