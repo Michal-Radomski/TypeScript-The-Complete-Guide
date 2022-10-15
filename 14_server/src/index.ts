e3dc08735e3ca4c2e248387bfabd856fdc509a2a
Michal Radomski <michalra@op.pl>
2022-10-16 00:48:57 +0200

    Exercise 14 done

--- 14_server/src/index.ts
+++ 14_server/src/index.ts
@@ -39,3 +39,40 @@ app.listen({ port: port }, () => {
   // For testing only
   console.log("Current Time:", new Date().toLocaleTimeString());
 });
+
+// * Express.js Server with Classes
+// class Server {
+//   app: express.Express = express();
+//   constructor() {
+//     // Middlewares
+//     this.app.use(bodyParser.json());
+//     this.app.use(bodyParser.urlencoded({ extended: true }));
+//     this.app.use(morgan("combined"));
+//     this.app.use(
+//       cookieSession({
+//         name: "session",
+//         keys: [process.env.NODE_SECRET as string],
+//       })
+//     );
+//   }
+
+//   get() {
+//     this.app.get("/", (req: Request, res: Response) => {
+//       console.log("req.ip:", req.ip);
+//       res.send("<h1 style='color:green;text-align:center'>API is running</h1>");
+//     });
+//   }
+
+//   start(): void {
+//     const port = (process.env.PORT || 5000) as number;
+//     this.app.listen({ port: port }, () => {
+//       console.log(`Server is listening at http://localhost:${port}`);
+//       // For testing only
+//       console.log("Current Time:", new Date().toLocaleTimeString());
+//     });
+//   }
+// }
+
+// const server = new Server();
+// server.get();
+// server.start();
