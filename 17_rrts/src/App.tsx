ee6ee40b97daaf7aee0c49c2b30b831c02a71823
Michal Radomski <michalra@op.pl>
2022-10-17 01:32:03 +0200

    Exercise 17 - commit2

--- 17_rrts/src/App.tsx
+++ 17_rrts/src/App.tsx
@@ -2,8 +2,38 @@ import React from "react";
 
 import "./App.scss";
 
-function App() {
-  return <React.Fragment>App</React.Fragment>;
+// function App():JSX.Element {
+//   return <React.Fragment>App</React.Fragment>;
+// }
+
+interface AppProps {
+  color?: string;
+}
+
+class App extends React.Component<AppProps, {}> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button style={{ color: this.props.color }} onClick={this.onIncrement}>
+          Increment
+        </button>
+        <button style={{ color: this.props.color }} onClick={this.onDecrement}>
+          Decrement
+        </button>
+        <br />
+        Counter State: {this.state.counter}
+      </div>
+    );
+  }
 }
 
 export default App;
